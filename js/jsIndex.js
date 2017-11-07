54437b582436bd0f02a1e3bcce476492e601689f
josevasquezr <javierv.ramos@gmail.com>
2017-11-06 21:26:46 -0600

    Estructura de arreglos

--- js/jsIndex.js
+++ js/jsIndex.js
@@ -1,4 +1,52 @@
 window.onload = function(){
+	var categorias = new Array("Ropa", "Alimentos");
+	var productos = new Array(
+							new Array("Camisas", "Pantalones", "Calsonetas"), 
+							new Array("Frutas", "Verduras", "Condimentos")
+							);
+	var marcas = new Array(
+						new Array(
+							new Array("Pacer", "Demo"), 
+							new Array("Pepe", "Massimo"), 
+							new Array("Nike", "Adidas")),
+						new Array(
+							new Array("Manzana", "Pera"), 
+							new Array("Pataste", "Papa"), 
+							new Array("Especias", "Canela"))
+						);
 
+	var ventasMensuales = new Array(
+							new Array(
+								new Array(
+									new Array(20, 31, 42, 25), 
+									new Array(12, 10, 5, 15)
+								), 
+								new Array(
+									new Array(50, 60, 25, 33), 
+									new Array(3, 16, 8, 7)
+								), 
+								new Array(
+									new Array(40, 76, 22, 15), 
+									new Array(41, 28, 24, 11)
+								)
+							),
+							new Array(
+								new Array(
+									new Array(2, 6, 8, 1), 
+									new Array(12, 22, 2, 6)
+								), 
+								new Array(
+									new Array(4, 7, 16, 45), 
+									new Array(10, 40, 25, 2)
+								), 
+								new Array(
+									new Array(18, 9, 4, 8), 
+									new Array(19, 7, 1, 4)
+								)
+							)
+						);
+
+	
+	
 
 }

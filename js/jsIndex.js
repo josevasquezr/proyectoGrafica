1031d6cefc837a96c0055a38a6afe74f5d3c3460
josevasquezr <javierv.ramos@gmail.com>
2017-11-07 01:36:33 -0600

    Detalles

--- js/jsIndex.js
+++ js/jsIndex.js
@@ -189,14 +189,14 @@ window.onload = function(){
 		            borderWidth: 0,
 		            dataLabels: {
 		                enabled: true,
-		                format: '{point.y:.1f}%'
+		                format: '{point.y:.1f}'
 		            }
 		        }
 		    },
 
 		    tooltip: {
 		        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-		        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+		        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> total<br/>'
 		    },
 
 		    series: [{
